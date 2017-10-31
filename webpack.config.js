e9dc21355fcdfb2634ce6c8f5a8a4c1ebfcef91d
Kay Christensen <kaychristensen@gmail.com>
2017-10-31 11:57:55 -0700

    Configure webpack.

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  // always running it from root directory
+  context: __dirname,
+  entry: './js/ClientApp.jsx',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    // specifies order of resolution of extensions
+    extensions: ['.js', '.jsx', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  module: {
+    rules: [
+      {
+        // any file with extension .js or .jsx, run through babel
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
