6972a2f9a8815318f47d77c125db868277a7cc1c
Kay Christensen <kaychristensen@gmail.com>
2017-11-01 15:04:53 -0700

    Include poster imgs.

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import preload from './data';
+import preload from '../data.json';
 
 const Search = () => (
-  <div className='search'>
-    <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
+  <div className="search">
+    {preload.shows.map(show => (
+      <div className="show-card">
+        <img alt={`${show.title} Show Poster`} src={`/public/img/posters/${show.poster}`} />
+        <div>
+          <h3>{show.title}</h3>
+          <h4>({show.year})</h4>
+          <p>{show.description}</p>
+        </div>
+
+      </div>
+    ))}
   </div>
 );
 
