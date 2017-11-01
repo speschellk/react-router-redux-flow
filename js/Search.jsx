782b5f88fca5feb73ec4e7359ef783d7d911c9d4
Kay Christensen <kaychristensen@gmail.com>
2017-11-01 15:12:39 -0700

    Add ShowCard component.

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import preload from '../data.json';
+import ShowCard from './ShowCard';
 
 const Search = () => (
   <div className="search">
     {preload.shows.map(show => (
-      <div className="show-card">
-        <img alt={`${show.title} Show Poster`} src={`/public/img/posters/${show.poster}`} />
-        <div>
-          <h3>{show.title}</h3>
-          <h4>({show.year})</h4>
-          <p>{show.description}</p>
-        </div>
-
-      </div>
+      <ShowCard show={show} />
     ))}
   </div>
 );
