df93baf89627a1b07d9bae8fed7bb6a7547ea132
Kay Christensen <kaychristensen@gmail.com>
2017-11-01 17:02:36 -0700

    Filter.

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
 
-const Search = () => (
-  <div className="search">
-    <div>
-      {preload.shows.map(show => (
-        <ShowCard
-          key={show.imdbID}
-          poster={show.poster}
-          title={show.title}
-          year={show.year}
-          description={show.description}
-        />
-      ))}
-    </div>
-  </div>
-);
+class Search extends Component {
+  state = {
+    searchTerm: ''
+  };
+
+  handleSearchTermChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => (
+              <ShowCard
+                key={show.imdbID}
+                poster={show.poster}
+                title={show.title}
+                year={show.year}
+                description={show.description}
+              />
+            ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
