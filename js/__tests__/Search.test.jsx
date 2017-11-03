dd7a58d4ec814855b97691b6d629bb4332237a0e
Kay Christensen <kaychristensen@gmail.com>
2017-11-03 10:39:29 -0700

    Add iframes.

--- js/__tests__/Search.test.jsx
+++ js/__tests__/Search.test.jsx
@@ -5,18 +5,18 @@ import { shallow } from 'enzyme'; // stubs out all the children components
 import preload from '../../data.json';
 
 test('Search renders correctly', () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows}/>);
   expect(component).toMatchSnapshot();
 });
 
 test('Search should render correct number of shows', () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows}/>);
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
 test('Search should render correct number of shows for search term', () => {
   const searchWord = 'black';
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows}/>);
   component.find('input').simulate('change', { target: { value: searchWord } });
   const showCount = preload.shows.filter(show =>
     `${show.title} ${show.description}`
