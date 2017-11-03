e5d1afea38845a8653c009312e0cf4b12f38ab4a
Kay Christensen <kaychristensen@gmail.com>
2017-11-03 09:50:52 -0700

    Add Details component and route.

--- js/App.jsx
+++ js/App.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
+import Details from './Details';
 
 const FourOhFour = () => <h1>404</h1>;
 
@@ -17,6 +18,7 @@ const App = () => (
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
         {/* If no match, render this component */}
+        <Route path="/details/:id" component={Details} />
         <Route component={FourOhFour} />
       </Switch>
     </div>
---
+++ js/Details.jsx
@@ -0,0 +1,11 @@
+// @flow
+
+import React from 'react';
+
+const Details = () => (
+  <div className="details">
+    <h1>THINGS</h1>
+  </div>
+);
+
+export default Details;
