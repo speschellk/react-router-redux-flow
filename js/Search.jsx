6b4608ef816ce2377666f9a922467c952f8b3019
Kay Christensen <kaychristensen@gmail.com>
2017-11-05 16:36:41 -0800

    Convert back to functional component for Redux.

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,53 +1,32 @@
 // @flow
 
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import ShowCard from './ShowCard';
 import Header from './Header';
 
-class Search extends Component {
-  state = {
-    searchTerm: ''
-  };
-  props: {
-    shows: Array<Show>
-  };
-  handleSearchTermChange = (
-    event: SyntheticKeyboardEvent & { target: HTMLInputElement }
-  ) => {
-    this.setState({ searchTerm: event.target.value });
-  };
+const Search = (props: { searchTerm: string, shows: Array<Show> }) => (
+  <div className="search">
+    <Header
+      searchTerm={props.searchTerm}
+      showSearch
+      handleSearchTermChange={props.handleSearchTermChange}
+    />
+    <div>
+      {props.shows
+        .filter(
+          show =>
+            `${show.title} ${show.description}`
+              .toUpperCase()
+              .indexOf(props.searchTerm.toUpperCase()) >= 0
+        )
+        .map(show => <ShowCard key={show.imdbID} {...show} />)}
+    </div>
+  </div>
+);
 
-  render() {
-    return (
-      <div className="search">
-        <Header
-          searchTerm={this.state.searchTerm}
-          showSearch
-          handleSearchTermChange={this.handleSearchTermChange}
-        />
-        <div>
-          {this.props.shows
-            .filter(
-              show =>
-                `${show.title} ${show.description}`
-                  .toUpperCase()
-                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
-            )
-            .map(show => (
-              <ShowCard
-                key={show.imdbID}
-                poster={show.poster}
-                title={show.title}
-                year={show.year}
-                description={show.description}
-                imdbID={show.imdbID}
-                {...show}
-              />
-            ))}
-        </div>
-      </div>
-    );
-  }
-}
+const mapStateToProps = state => ({
+  searchTerm: state.searchTerm
+});
 
-export default Search;
+export default connect(mapStateToProps)(Search);
