6758ec06bd176a7ebb9170387811a9e6993cf06b
Kay Christensen <kaychristensen@gmail.com>
2017-11-06 16:07:11 -0800

    Configure reducers and actions for thunk.

--- js/reducers.js
+++ js/reducers.js
@@ -1,7 +1,7 @@
 // @flow
 
 import { combineReducers } from 'redux';
-import { SET_SEARCH_TERM } from './actions';
+import { SET_SEARCH_TERM, ADD_API_DATA } from './actions';
 
 const searchTerm = (state = '', action: Action) => {
   if (action.type === SET_SEARCH_TERM) {
@@ -10,8 +10,17 @@ const searchTerm = (state = '', action: Action) => {
   return state;
 };
 
+const apiData = (state = {}, action: Action) => {
+  if (action.type === ADD_API_DATA) {
+    return Object.assign({}, state, {
+      [action.payload.imdbID]: action.payload
+    });
+  }
+  return state;
+};
+
 // searchTerm very siloed to this reducer and
 // others don't need to worry about it
-const rootReducer = combineReducers({ searchTerm });
+const rootReducer = combineReducers({ searchTerm, apiData });
 
 export default rootReducer;
--- js/store.js
+++ js/store.js
@@ -1,8 +1,20 @@
 // @flow
 
-import { createStore } from 'redux';
-import reducer from './reducers'
+import { createStore, compose applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  // augment Redux capabilities by giving it middleware
+  compose(
+    applyMiddleware(thunk),
+    typeof window === 'object' &&
+      typeof window.devToolsExtension !== 'undefined'
+      ? window.devToolsExtension()
+      // identity function
+      : f => f
+  )
+);
 
 export default store;
