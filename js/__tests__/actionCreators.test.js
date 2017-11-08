2a16b37cd8f06123085da1a6b4329d7649faea3b
Kay Christensen <kaychristensen@gmail.com>
2017-11-08 15:16:07 -0800

    Add api data mock test.

--- js/__tests__/actionCreators.test.js
+++ js/__tests__/actionCreators.test.js
@@ -1,20 +1,46 @@
 // @flow
 
-import { setSearchTerm, addAPIData } from '../actionCreators';
+import { setSearchTerm, addAPIData, getAPIDetails } from '../actionCreators';
+import moxios from 'moxios';
+
+const strangerThings = {
+  title: 'Stranger Things',
+  year: '2016–',
+  description: 'When a young boy disappears, his mother, a police chief, and his friends must confront terrifying forces in order to get him back.',
+  poster: 'st.jpg',
+  imdbID: 'tt4574334',
+  trailer: '9Egf5U8xLo8',
+  rating: '8.6'
+};
 
 test('setSearchTerm', () => {
   expect(setSearchTerm('New York')).toMatchSnapshot();
 });
 
 test('addAPIData', () => {
-expect(addAPIData({
-      "title": "Stranger Things",
-      "year": "2016–",
-      "description":
-        "When a young boy disappears, his mother, a police chief, and his friends must confront terrifying forces in order to get him back.",
-      "poster": "st.jpg",
-      "imdbID": "tt4574334",
-      "trailer": "9Egf5U8xLo8",
-      "rating": "8.6"
-    })).toMatchSnapshot();
+  expect(addAPIData(strangerThings)).toMatchSnapshot();
+});
+
+test('getAPIDetails', (done: Function) => {
+  const dispatchMock = jest.fn();
+  moxios.withMock(() => {
+    const thunk = getAPIDetails(strangerThings.imdbID);
+    thunk(dispatchMock);
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request
+        .respondWith({
+          status: 200,
+          response: strangerThings
+        })
+        .then(() => {
+          expect(request.url).toEqual(
+            `http://localhost:3000/${strangerThings.imdbID}`
+          );
+          expect(dispatchMock).toBeCalledWith(addAPIData(strangerThings));
+          done();
+        });
+    });
+  });
+  expect(addAPIData(strangerThings)).toMatchSnapshot();
 });
