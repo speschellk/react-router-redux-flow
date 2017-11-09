bc8fb2abacab170940e54f1248be373af51f12f0
Kay Christensen <kaychristensen@gmail.com>
2017-11-09 10:54:16 -0800

    Add AsyncRoute.

--- js/App.jsx
+++ js/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import type { Match } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import Landing from './Landing';
+import AsyncRoute from './AsyncRoute';
 import Search from './Search';
 import Details from './Details';
 import preload from '../data.json';
@@ -14,11 +14,17 @@ const FourOhFour = () => <h1>404</h1>;
 
 const App = () => (
   <BrowserRouter>
-  {/* Puts Redux on context so we can use it elsewhere */}
+    {/* Puts Redux on context so we can use it elsewhere */}
     <Provider store={store}>
       <div className="app">
         <Switch>
-          <Route exact path="/" component={Landing} />
+          <Route
+            exact
+            path="/"
+            component={props => (
+              <AsyncRoute props={props} loadingPromise={import('./Landing')} />
+            )}
+          />
           <Route
             path="/search"
             component={props => <Search shows={preload.shows} {...props} />}
