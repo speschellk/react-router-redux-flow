78cd665a275e19d4d26252e861c8295c1dabc704
Kay Christensen <kaychristensen@gmail.com>
2017-11-09 11:06:15 -0800

    All async routing, code split.

--- js/App.jsx
+++ js/App.jsx
@@ -27,18 +27,30 @@ const App = () => (
           />
           <Route
             path="/search"
-            component={props => <Search shows={preload.shows} {...props} />}
+            component={props => (
+              <AsyncRoute
+                props={Object.assign({ shows: preload.shows }, props)}
+                loadingPromise={import('./Search')}
+              />
+            )}
           />
           <Route
             path="/details/:id"
-            component={(props: { match: Match }) => (
-              <Details
-                show={preload.shows.find(
-                  show => props.match.params.id === show.imdbID
-                )}
-                {...props}
-              />
-            )}
+            component={(props: { match: Match }) => {
+              const selectedShow = preload.shows.find(
+                (show: Show) => props.match.params.id === show.imdbID
+              );
+
+              return (
+                <AsyncRoute
+                  props={Object.assign(
+                    { show: selectedShow, match: {} },
+                    props
+                  )}
+                  loadingPromise={import('./Details')}
+                />
+              );
+            }}
           />
           <Route component={FourOhFour} />
         </Switch>
